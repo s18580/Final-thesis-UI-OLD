b384b6fc1d785ea5c084a50d34315e7bb83edcfa
Hubert <golab.mlody@gmail.com>
2021-11-07 12:01:08 +0100

    place userMenu in HeaderBar

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,21 +1,37 @@
 
 <template>
     <div id="background">
+      <div id="logo">
+      </div>
+      <div id="userMenu">
+        <UserMenu/>
+      </div>
     </div>
 </template>
 
 <script>
+import UserMenu from "./UserMenu.vue";
 
 export default {
   name: 'HeaderBar',
+  components: {
+    UserMenu
+  }
 }
 </script>
 
 <style scoped>
-#background{
+#background {
   display: block;
   min-height: 60px;
   width: 100%;
-  background: #002244
+  background: #002244;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+#userMenu {
+  margin: 15px;
 }
 </style>
