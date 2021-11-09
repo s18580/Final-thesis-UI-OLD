0e964e9174ffb9d4bcf0d82497b6ac0d5e455bd8
Hubert <golab.mlody@gmail.com>
2021-11-09 22:14:33 +0100

    create list of routes to make in MenuBar component

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,6 +1,73 @@
 
 <template>
     <div id="background">
+      <ul>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Strona główna - Admin
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Zarządzanie kontami
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Stałe programowe
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Kopie zapasowe
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Strona główna - Pracownik
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Zamówienia
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Klienci
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Dostawcy
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Wyceny
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Pracownicy
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Statystyki
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Wyszukaj osobę kontaktową
+          </div>
+        </li>
+        <li>
+          <div v-if="hasRole()" class="menuLink">
+            Strona główna - Księgowa
+          </div>
+        </li>
+      </ul>
     </div>
 </template>
 
@@ -8,6 +75,11 @@
 
 export default {
   name: 'MenuBar',
+  methods: {
+    hasRole: function(){
+      return true; //ToDo: checking role in VueX store
+    }
+  }
 }
 </script>
 
@@ -17,5 +89,20 @@ export default {
   height: 100%;
   position: fixed;
   background: linear-gradient(to right top, #000000, #007FFF);
+}
+.menuLink{
+  padding: 15px;
+  color: white;
+  text-align: center;
+  border-bottom: 0.1rem solid black;
+}
+
+#background ul{
+  padding: 0;
+  margin: 0;
+}
+
+#background ul li{
+  list-style-type: none;
 }
 </style>
